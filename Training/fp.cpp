e94b2411bc9770f9d8df952b90cbf95a46f8b167
RexO77 <nischal.skanda07@gmail.com>
2024-07-02 11:57:49 +0530

    Pattern1

--- Training/fp.cpp
+++ Training/fp.cpp
@@ -1,6 +1,27 @@
-#include<stdio.h>
-int main(){
-    printf("Hello world");
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the size");
+    scanf("%d",&n);
+    for (int i = 1; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            printf(" ");
+        }
+        for (int k = 1; k <= (2 * i - 1); k++) {
+            printf("*");
+        }
+        printf("\n");
+}
+    for (int i = n; i >= 0; i--) {
+        for (int j = i; j < n; j++) {
+            printf(" ");
+        }
+        for (int k = 1; k <= (2 * i - 1); k++) {
+            printf("*");
+        }
+        printf("\n");
+}
     return 0;
 }
-//test seq
