4dd4e193f7dfa6e91aa6029e73d7061fff9ba993
RexO77 <nischal.skanda07@gmail.com>
2024-07-03 10:18:16 +0530

    Swap functions

--- Training/swap.cpp
+++ Training/swap.cpp
@@ -1,15 +1,21 @@
 #include<iostream>
 using namespace std;
-void swap(int a, int b){
-    int temp;
-    a = temp;
-    b = a;
-    temp= b;
+void swap(int &a, int &b){
+    int temp=a;
+    a = b;
+    b=temp;
+}
+void swap2(int *a,int *b){
+    int temp = *a;
+    *a = *b;
+    *b =temp;
 }
 int main(){
     int a=10 , b=20;
-    cout<<"Before Swap : "<<a<<b;
+    cout<<"Before Swap : "<<a<<b<<endl;
     swap(a,b);
-    cout<<"After Swap : "<<a<<b;
+    cout<<"After Swap : "<<a<<b<<endl;
+    swap2(&a,&b);
+    cout<<"Using Pointer Swap : "<<a<<b<<endl;
     return 0;
 }
