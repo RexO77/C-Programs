12626c0b2f0d8e6b6a54657c1a2ba1707d61cb08
RexO77 <nischal.skanda07@gmail.com>
2024-07-03 12:37:00 +0530

    Sum of N natural Numbers using recursion

--- Training/SumofN.cpp
+++ Training/SumofN.cpp
@@ -1,13 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n,sum=0;
+    long long n,sum=0;
     cout<<"Enter The Value of N : ";
     cin>>n;
-    for (int i =0; i<=n ; i++){
+    for (long long i =0; i<=n ; i++){
         sum += i;
     }
     cout<<"Sum is : "<<sum<<endl;
     return 0;
 }
-
