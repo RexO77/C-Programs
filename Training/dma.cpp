8028e1d6478b0d986860b7600dbe656415bf6c81
RexO77 <nischal.skanda07@gmail.com>
2024-07-04 12:14:20 +0530

    DMA  Demo

--- Training/dma.cpp
+++ Training/dma.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int *ptr = new int;
 
-    delete ptr;//it is a good practice to use free due to memory leak 
+    delete ptr;//it is a good practice to use free due to memory leak but in c++ it is automatically handled
     ptr = NULL;
     return 0;
 } // namespace std
