8bd5137752f68dc6e6d1b359d0c7b62150fbe02d
RexO77 <nischal.skanda07@gmail.com>
2024-07-04 11:13:14 +0530

    Pointers Demo

---
+++ Training/test.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i =18;
+    int *ptr;
+    cout<<i<<endl;
+    cout<<ptr<<endl;
+    cout<<&i<<endl;
+    cout<<&ptr<<endl;
+    cout<<*ptr<<endl;
+    return 0;
+}
