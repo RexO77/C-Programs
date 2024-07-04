f810f0cede0de23a008c9bae312eddc45e43fa33
RexO77 <nischal.skanda07@gmail.com>
2024-07-04 12:22:45 +0530

    Double pointer Demo

--- Training/dma.cpp
+++ Training/dma.cpp
@@ -1,9 +1,20 @@
 #include<iostream>//Danglind pointer Demo using the concept of DMA 
 using namespace std;
-int main(){
-    int *ptr = new int;
+// int main(){
+//     int *ptr = new int;
 
-    delete ptr;//it is a good practice to use free due to memory leak but in c++ it is automatically handled
-    ptr = NULL;
+//     delete ptr;//it is a good practice to use free due to memory leak but in c++ it is automatically handled
+//     ptr = NULL;
+//     return 0;
+// } // namespace std
+int main(){
+    int i = 50;
+    int *ptr;
+    int **dptr;
+    int ***ddptr;
+    ptr = &i;
+    dptr = &ptr;
+    ddptr = &dptr;
+    cout<<i<<*ptr<<**dptr<<***ddptr<<endl<<&ddptr<<endl;
     return 0;
-} // namespace std
+}
