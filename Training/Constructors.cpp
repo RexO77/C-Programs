c7ca886a85d02f6e57114505d61ec4e6c472cdd4
RexO77 <nischal.skanda07@gmail.com>
2024-07-05 10:56:37 +0530

    Constructors Demo

---
+++ Training/Constructors.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class Student{
+    public:
+    string Name;
+    int RollNo;
+
+    Student(string name,int rollNo){
+        Name=name;
+        RollNo=rollNo;
+    }
+    void printdata(){
+        cout<<"Student Name is : "<<Name<<endl;
+        cout<<"Roll Number is  : "<<RollNo<<endl;
+    }
+};
+int main(){
+    Student s1("Nischal",77);
+    Student s2("Harsha",82);
+    s1.printdata();
+    s2.printdata();
+    return 0;
+}
