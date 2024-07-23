9f4376e2500df3e2c17abd2e7f792b0cea91a756
RexO77 <nischal.skanda07@gmail.com>
2024-07-23 21:05:38 +0530

    Sum of Array

--- LeetCode/SumofArr.cpp
+++ LeetCode/SumofArr.cpp
@@ -8,3 +8,9 @@ int sumOfArr(int arr[], int n) {
     }
     return sum;
 }
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr)/sizeof(arr[0]); //5
+    cout<<sumOfArr(arr, n); //15
+    return 0;
+}// Time Complexity: O(n) space Complexity: O(1)
