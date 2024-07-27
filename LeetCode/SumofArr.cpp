49a64ce2fcb43d2ba0d95163951b4cccb6a887e8
RexO77 <nischal.skanda07@gmail.com>
2024-07-27 21:03:15 +0530

    Sum of Array

--- LeetCode/SumofArr.cpp
+++ LeetCode/SumofArr.cpp
@@ -15,4 +15,3 @@ int main() {
     return 0;
 }// Time Complexity: O(n) space Complexity: O(1)
 //The above code snippet is a simple program to find the sum of elements in an array. The function sumOfArr takes an array and its size as input and returns the sum of all elements in the array. The main function initializes an array and calls the sumOfArr function to find the sum of the array elements. The time complexity of the sumOfArr function is O(n) as it iterates through all elements of the array, and the space complexity is O(1) as it uses a constant amount of memory. The program outputs the sum of the array elements, which is 15 in this case.
-//test 1
