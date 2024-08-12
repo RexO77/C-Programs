4840b9146b7dced94fc5385bd0a2d7a42af36645
RexO77 <nischal.skanda07@gmail.com>
2024-08-12 21:19:01 +0530

    Leet stuff

--- LeetCode/TwoSum.cpp
+++ LeetCode/TwoSum.cpp
@@ -18,7 +18,7 @@ int twosum(vector<int>& nums, int target) {
 int main() {
     vector<int> nums = {2, 7, 11, 15};
     int target = 9;
-    vector<int> result = twosum(nums, target);
+    vector<int> result = twosum(nums, {target});
     for (int i = 0; i < result.size(); i++) {
         cout << result[i] << " ";
     }
