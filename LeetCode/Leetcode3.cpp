2948232c9f41d1c82b1f9b57f163111f445076b2
RexO77 <nischal.skanda07@gmail.com>
2024-08-20 21:35:37 +0530

    Leet stuff

--- LeetCode/Leetcode3.cpp
+++ LeetCode/Leetcode3.cpp
@@ -1,1 +1,19 @@
 //easy leetcode problem to solve
+//array addition 
+#include<iostream>
+using namespace std;
+int maxProfit(int prices[], int n) {
+    int profit = 0;
+    for(int i=1; i<n; i++) {
+        if(prices[i] > prices[i-1]) {
+            profit += prices[i] - prices[i-1];
+        }
+    }
+    return profit;
+}
+int main() {
+    int prices[] = {7, 1, 5, 3, 6, 4};
+    int n = sizeof(prices)/sizeof(prices[0]); //6
+    cout<<maxProfit(prices, n); //7
+    return 0;
+}
