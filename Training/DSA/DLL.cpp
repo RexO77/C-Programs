53f4b15fa286a548ca00b82cfc0033f5cca3ab51
RexO77 <nischal.skanda07@gmail.com>
2024-09-04 10:04:00 +0530

    Doubly linked list using C++

--- Training/DSA/DLL.cpp
+++ Training/DSA/DLL.cpp
@@ -1,14 +1,52 @@
 #include<iostream>
-using namespace std
+using namespace std;
 
-void print(int n){
-    if(n==0){
-        return;
+class Node {
+    public:
+    char data;
+    Node *next;
+    Node *prev;
+
+    Node(char data) {
+        this->prev = NULL;
+        this->data = data;
+        this->next = NULL;
     }
-    printf("%d",n%2);
-    print(n/2);
-}
-int main(){
-    print(12);
+};
+
+class Linkedlist {
+    public:
+    Node *head;
+    Linkedlist() {
+        this->head = NULL;
+    }
+
+    void insertAtHead(char val) {
+        Node *newNode = new Node(val);
+        newNode->next = head;
+        if (head != NULL) {
+            head->prev = newNode;
+        }
+        head = newNode; // Update the head to the new node
+    }
+
+    void display() {
+        Node *cur = head;
+        while (cur != NULL) {
+            cout << cur->data << " -> ";
+            cur = cur->next;
+        }
+        cout << "Null" << endl;
+    }
+};
+
+int main() {
+    Linkedlist ll;
+    ll.insertAtHead('A');
+    ll.insertAtHead('B');
+    ll.insertAtHead('C');
+    ll.insertAtHead('D');
+    ll.insertAtHead('E');
+    ll.display();
     return 0;
 }
