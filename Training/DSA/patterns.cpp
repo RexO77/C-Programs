9de86f159000cfe1d17d2ce17f5d68b1695bba31
RexO77 <nischal.skanda07@gmail.com>
2024-09-07 22:34:07 +0530

    Patterns

--- Training/DSA/patterns.cpp
+++ Training/DSA/patterns.cpp
@@ -4,6 +4,11 @@ int main(){
     int n;
     cout<<"Enter N value : "<<endl;
     cin>>n;
-    for(int i=0  ; i < n; i = i+1)
+    for(int i=0  ; i < n; i = i+1){
+        cout<<" * ";
+        for(int j = i; j<=n ; j= j+1){
+            cout<<" * ";
+        }cout<<""<<endl;
+    }
     return 0;
 }
