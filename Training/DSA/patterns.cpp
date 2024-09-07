d69f87e776884ddf2ff11090f54d53390a3cc02a
RexO77 <nischal.skanda07@gmail.com>
2024-09-07 22:32:01 +0530

    Patterns

--- Training/DSA/patterns.cpp
+++ Training/DSA/patterns.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
-    return;
+    int n;
+    cout<<"Enter N value : "<<endl;
+    cin>>n;
+    for(int i=0  ; i < n; i = i+1)
+    return 0;
 }
