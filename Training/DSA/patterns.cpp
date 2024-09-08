1100be7c905d43463b51ab9ccc25dc84198562c6
RexO77 <nischal.skanda07@gmail.com>
2024-09-08 21:27:37 +0530

    Pattern 8,9,10,11

--- Training/DSA/patterns.cpp
+++ Training/DSA/patterns.cpp
@@ -1,48 +1,48 @@
 #include<iostream>
 using namespace std;
-void p1(int n){
+void p1(int n){ //pattern 1
     for(int i=0 ;i<n;i++){
         for(int j =0; j<n;j++){
             cout<<"*";
         }cout<<endl;
     }
 }
-void p2(int n){
+void p2(int n){ //pattern 1 with numbers
     for(int i= 0 ;i<=n ;i++){
         for(int j=n-i+1 ; j<=n ; j++){
             cout<<"*";
         }cout<<endl;
     }
 }
-void p3(int n){
+void p3(int n){ //pattern 2 with numbers
     for(int i= 0 ;i<=n ;i++){
         for(int j=n-i+1 ; j<=n ; j++){
             cout<<" "<<i;
         }cout<<endl;
     }
 }
-void p4(int n){
+void p4(int n){ //triangle pattern
     for(int i= 0 ;i<=n ;i++){
         for(int j=i+1 ; j<=n ; j++){
             cout<<"*";
         }cout<<endl;
     }
 }
-void p5(int n){
+void p5(int n){ //triangle pattern
     for(int i= 1 ;i<=n ;i++){
         for(int j=1 ; j<=i ; j++){
             cout<<"*";
         }cout<<endl;
     }
 }
-void p6(int n){
+void p6(int n){ //pattern 3 with numbers
     for(int i=0 ;i<=n ;i++){
         for(int j= i+1 ; j<=n ; j++){
             cout<<" "<<j;
         }cout<<endl;
     }
 }
-void p7(int n){
+void p7(int n){ //triangle
     for(int i = 0;i<n;i++){
         //space
         for(int j = 0;j<n-i-1;j++){
@@ -58,7 +58,79 @@ void p7(int n){
         }cout<<endl;
     }
 }
+void p8(int n){ //inverted triangle
+    for(int i =0;i<n;i++){
+        //spaces
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        //stars
+        for(int j=0;j<2*n-(2*i+1);j++){
+            cout<<"*";
+        }
+        //spaces
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+void p9(int n){ //combination of 2 triangle patter ie p7 & p8
+    for(int i =0;i<n;i++){
+        //spaces
+        for(int j =0 ;j<n-i;j++){
+            cout<<" ";
+        }
+        //stars
+        for(int j = 0;j<2*i-1;j++){
+            cout<<"*";
+        }
+        //spaces
+        for(int j =0 ;j<n-i;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    for(int i =0;i<n;i++){
+        //spaces
+        for(int j = 0;j <i; j++){
+            cout<<" ";
+        }
+        //stars
+        for(int j = 0;j<2*n-(2*i+1);j++){
+            cout<<"*";
+        }
+        //spaces
+        for(int j = 0;j<i; j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+void p10(int n){// left Arrow by combining 2 triangle
+    for(int i = 0;i<n;i++){
+        for(int j =0 ;j<=i;j++){
+            cout<<"*";
+        }cout<<endl;
+    }
+    for(int i =0;i<=n;i++){
+        for(int j = i;j<=n;j++){
+            cout<<"*";
+        } cout<<endl;
+    }
+}
+void p11(int n){//left Arrow without combining 2 triangle
+    for(int i =0;i<2*n-1;i++){
+        int star = i;
+        if(i>=n){
+            star = 2*n-i-2;
+        }
+        for( int j=0;j<star ;j++){
+            cout<<"*";
+        }cout<<endl;
+    }
+}
 int main(){
-    p7(5);
+    p11(5);
     return 0;
 }
