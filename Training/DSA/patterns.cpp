341d8b306bc1d247f1cd36dd5cc6bcfaff32d2d6
RexO77 <nischal.skanda07@gmail.com>
2024-09-08 11:33:33 +0530

    Pattern 7

--- Training/DSA/patterns.cpp
+++ Training/DSA/patterns.cpp
@@ -43,13 +43,22 @@ void p6(int n){
     }
 }
 void p7(int n){
-    for(int i= 0 ;i<=n ;i++){
-        for(int j=n-i+1 ; j<=n ; j++){
+    for(int i = 0;i<n;i++){
+        //space
+        for(int j = 0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        //stars
+        for(int j=0; j< 2*i+1;j++){
             cout<<"*";
+        }
+        //space
+        for(int j = 0;j<n-i-1;j++){
+            cout<<"";
         }cout<<endl;
     }
 }
 int main(){
-    p6(5);
+    p7(5);
     return 0;
 }
